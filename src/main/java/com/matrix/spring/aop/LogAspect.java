7641b0bb3bc5439dee6fa43ee49059e6a7f42811
hongyunyoung <hongyun0@naver.com>
2018-08-17 22:46:32 +0900

    로그 생성 코드 AOP 사용해서 분리.

---
+++ src/main/java/com/matrix/spring/aop/LogAspect.java
@@ -0,0 +1,42 @@
+package com.matrix.spring.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import com.matrix.spring.task.daily.DailyDTO;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+	@Pointcut(value = "bean(*Controller)")
+	private void controllerPointcut() {}
+	
+	@AfterThrowing(pointcut = "controllerPointcut()", throwing = "exception")
+	public void exceptionWarn(JoinPoint joinPoint, Exception exception) {
+		log.warn("WARN! {} : '{}' [{}]", joinPoint.getSignature(), exception.getMessage(), exception.getStackTrace()[0]);
+	}
+	
+	@Pointcut(value = "execution(* com.matrix.spring.user.UserController.login(*, *, *))")
+	private void loginPointcut() {}
+	
+	@AfterReturning(pointcut = "loginPointcut() && args(userId, *, *)")
+	public void loginInfo(JoinPoint joinPoint, String userId) {
+		log.info("INFO [{}] login.", userId);
+	}
+	
+	@Pointcut(value = "execution(* com.matrix.spring.task.daily.DailyController.assignTask(*, *, *))")
+	private void assignPointcut() {}
+	
+	@AfterReturning(pointcut = "assignPointcut() && args(dailyDTO, *, userId)")
+	public void assignInfo(JoinPoint joinPoint, DailyDTO dailyDTO, String userId) {
+		log.info("INFO [{}] assign '{}' to [{}].", userId, dailyDTO.getDailyTask(), dailyDTO.getAssignDetail());
+	}
+	
+}
