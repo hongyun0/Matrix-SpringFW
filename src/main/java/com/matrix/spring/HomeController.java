59fcba3a3e09bbd9ecda1725e7c9c1d1ffdc8b7c
hongyunyoung <hongyun0@naver.com>
2018-08-17 22:44:18 +0900

    직접적인 로그 생성 코드들 삭제.

--- src/main/java/com/matrix/spring/HomeController.java
+++ src/main/java/com/matrix/spring/HomeController.java
@@ -7,12 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * Handles requests for the application home page.
  */
-@Slf4j
 @Controller
 public class HomeController {
 	/**
