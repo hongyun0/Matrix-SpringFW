22dc62435540cef686a4e4a915a81cd4f8d70ea7
hongyunyoung <hongyun0@naver.com>
2018-08-18 21:47:28 +0900

    Admin 권한 확인 인터셉터 기능 추가.

---
+++ src/main/java/com/matrix/spring/interceptor/AdminInterceptor.java
@@ -0,0 +1,39 @@
+package com.matrix.spring.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AdminInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		if (session.getAttribute("userId") == null 
+				|| session.getAttribute("branchSeq") == null 
+				|| !((String)session.getAttribute("type")).equals("admin")
+				|| session.getAttribute("adminSeq") == null) {
+			response.sendRedirect(request.getContextPath() + "/login");
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		
+	}
+	
+
+}
